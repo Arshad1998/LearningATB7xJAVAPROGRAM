df8cc3f6cecb0947f3e7626f7d6b3172f5f4423f
Arshad1998 <arshadshafeeq09@gmail.com>
2024-07-14 10:12:13 +0530

    All Files Included

---
+++ src/ex_13072024/Lab016.java
@@ -0,0 +1,13 @@
+package ex_13072024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        byte b  = 10;
+        System.out.println(b);
+        // JVM -> brother who will execute the main method
+        // 10 - Decimal (We write in Decimal and it will converted to Binary and get stored in memory)
+        // Machine(Binary) -> 00001010 - 8 bits = 1 Byte
+        long distance_moon_earth = 3844000000000000L;
+        System.out.println(distance_moon_earth);
+    }
+}
---
+++ src/ex_13072024/Lab018.java
@@ -0,0 +1,25 @@
+package ex_13072024;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        //byte b = 128; //-127 to 127
+
+        char c = 'A';
+        System.out.println(c);
+
+        char c1 = '$';
+        char c2 = '&';
+        System.out.println(c1);
+        System.out.println(c2);
+
+        char c3 = '\n';
+        System.out.println(c3);
+
+        char c4 = '\u1F6A';  //:)
+        System.out.println(c4);
+        // :), :(, :D .;) -> unicode chars
+
+        char c5 = '\u1F4A';
+        System.out.println(c5);
+    }
+}
