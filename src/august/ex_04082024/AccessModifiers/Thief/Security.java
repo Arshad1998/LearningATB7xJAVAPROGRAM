5b8854ec9874dc1fe2dd83d424f8311edfb45ec4
Arshad1998 <arshadshafeeq09@gmail.com>
2025-10-04 12:19:14 +0530

    Minor change during Revision

---
+++ src/august/ex_04082024/AccessModifiers/Thief/Security.java
@@ -0,0 +1,12 @@
+package august.ex_04082024.AccessModifiers.Thief;
+
+import august.ex_04082024.AccessModifiers.Police.Cop;
+
+public class Security extends Cop {
+
+    void NeedGun(){
+        CanIShoot(); // Can access the protected method from parent class in different Package
+    }
+
+
+}
