14ddc8aac1bfd91fef996f7764b531f12a61e373
Arshad1998 <arshadshafeeq09@gmail.com>
2025-10-04 12:30:54 +0530

    Minor change during Revision

--- src/august/ex_04082024/AccessModifiers/Thief/Security.java
+++ src/august/ex_04082024/AccessModifiers/Thief/Security.java
@@ -5,7 +5,7 @@ import august.ex_04082024.AccessModifiers.Police.Cop;
 public class Security extends Cop {
 
     void NeedGun(){
-        CanIShoot(); // Can access the protected method from parent class in different Package
+        CanIShoot(); // Can access the protected method from parent class in different Package but not default method
     }
 
 
